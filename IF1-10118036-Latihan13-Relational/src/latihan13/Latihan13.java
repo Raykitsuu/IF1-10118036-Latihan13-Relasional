360334725592704c3a029cbb561a72bebc6c3c02
Adrian C. Senna <55816324+Raykitsuu@users.noreply.github.com>
2019-09-26 19:27:43 +0700

    Add files via upload

---
+++ IF1-10118036-Latihan13-Relational/src/latihan13/Latihan13.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan13;
+
+/**
+ *
+ * @author
+ * NAMA			: Adrian C. Senna
+ * KELAS		: IF-1
+ * NIM			: 10118036
+ * Deskripsi Program	: Program ini berisi program untuk memperlihatkan operasi relasional.
+ *
+ */
+public class Latihan13 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		
+		
+		int bil1 = 10;
+		int bil2 = 20;
+		System.out.println("a == b = " + (bil1 == bil2));
+		System.out.println("a != b = " + (bil1 != bil2));
+		System.out.println("a > b = " + (bil1 > bil2));
+		System.out.println("a < b = " + (bil1 < bil2));
+		System.out.println("b >= a = " + (bil2 >= bil1));
+		System.out.println("b <= a = " + (bil2 <=bil1));
+	}	
+	
+}
